7600266339fee4df8b368f36b734fddc397e8737
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-08-23 12:57:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,29 @@ client.on('ready', () => {
 });
 
 client.on("message", async message => {
+    
+exports.run = (client, message, args, level) => {  
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Je ne sais pas trouver cette personne");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Tu n\'as pas la permission");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Je ne peux pas ban cette personne");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setTitle("Ban")
+    .setColor("#bc0000")
+    .addField("Utilisateur ban", `${bUser}\nID ${bUser.id}`)
+    .addField("Banni par", `<@${message.author.id}>\nID ${message.author.id}`)
+    .addField("Banni dans", message.channel)
+    .addField("Temps", message.createdAt)
+    .addField("Reason", bReason);
+
+    let canal = message.guild.channels.find(`name`, "🔩logs");
+
+    message.guild.member(bUser).ban(bReason);
+    canal.send(banEmbed);
+       }
+    
 });
 
 client.login(process.env.TOKEN);
